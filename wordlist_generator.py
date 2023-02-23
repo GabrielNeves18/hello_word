f9c6919df072d2e6980a4aac96fd199fa86234d7
GabrielNeves18 <42821293+GabrielNeves18@users.noreply.github.com>
2023-02-23 15:36:09 +0000

    fddrdd

--- wordlist_generator.py
+++ wordlist_generator.py
@@ -4,10 +4,10 @@
 #    This program comes with ABSOLUTELY NO WARRANTY; for details read LICENSE.
 #    This is free software, and you are welcome to redistribute it
 #    under certain conditions; you can read LICENSE for details.
-#
+##ghfdytdytddyd
 
 
-
+#ghfdytdytddyd
 
 
 def menu():
